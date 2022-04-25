ef03b4b1bebe8cbf4c93785e6449a7e79cd67166
M1082878 <M1082878@MT-5CG14786HZ>
2022-04-25 12:04:51 +0530

    Welcome to Git

---
+++ welcome to git/hello/Welcome.java
@@ -0,0 +1,22 @@
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a; int sum =0 ;
+		for(int i=0;i<10;i++)
+		{
+			sum = sum+ i; 
+			if(sum<20)
+			{
+				System.out.println("Welcome to Git");
+			}
+			else
+			{
+				System.out.println(sum);
+			}
+		}
+	}
+
+}
